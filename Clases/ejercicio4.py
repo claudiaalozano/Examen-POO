561de1db6690a6cf838a0d01285f425e3f660949
claudiaalozano <clau.lozano.perez@gmail.com>
2022-03-24 09:48:04 +0100

    creo la carpeta clases y comienzo a hacer el ejercicio 4

---
+++ Clases/ejercicio4.py
@@ -0,0 +1,15 @@
+import random
+class cuenta_bancaria:
+    def retirar_dinero (self,saldo):
+        self.retirar = saldo - 78
+        print("Ha retirado 78 €, el dinero de su cuenta desciende a 9922€ .")
+    def ingresar_dinero (self,saldo):
+        self.ingreso = saldo 
+
+
+
+
+saldo = 10000
+id_1= random.randint(100000000000,999999999999)
+id_2= random.randint(100000000000,999999999999)
+id_3= random.randint(100000000000,999999999999)
