5e5c1e3840478bc4b693fc16d7e4db29dda27331
claudiaalozano <clau.lozano.perez@gmail.com>
2022-03-24 10:34:03 +0100

    cambios en el código creando una nueva función

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 if __name__ == "__main__":
-    print("Bienvenido al banco, van a comenzar las trasacciones:")
-    from Clases.ejercicio4 *
+    operacion = input(print("Bienvenido al banco, van a comenzar las trasacciones(retirar/ingresar/transferir):"))
+    from Clases.ejercicio4 import *
+
+    if operacion == "retirar":
+        ejercicio = cuenta_bancaria()
+        print(ejercicio.retirar_dinero())
+    if operacion == "ingresar":
+        ejercicio = cuenta_bancaria()
+        print(ejercicio.ingresar_dinero())
+    if operacion == "transferir":
+        ejercicio = cuenta_bancaria()
+        print(ejercicio.transferencia())
     
+    
+
